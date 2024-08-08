2cfd9edefb1fde0e0eaed329303f794361c2a700
manasimran <126086161+manasimran@users.noreply.github.com>
2024-08-08 14:30:44 +0500

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,38 @@
+export const footerLinks = [
+  {
+    title: "Contact Us",
+    links: [
+      { title: "Whattsapp", url: "https://wa.me/923331493112" },
+      { title: "Email", url: "/" },
+      { title: "Visit us", url: "https://www.google.com/maps/place/Brothers+Motors+%26+Rent+A+Car/@33.5822712,73.0273664,858m/data=!3m2!1e3!4b1!4m6!3m5!1s0xa7f62690b2d3de35:0xe0d9888b665e25a2!8m2!3d33.5822668!4d73.0299413!16s%2Fg%2F11w4nfjykw?entry=ttu" },
+      { title: "Phone", url: "tel:+923331493112" },
+    ],
+  },
+  {
+      title: "About Us",
+      links: [
+        { title: "How it works", url: "https://brotherrents.blogspot.com/2024/07/discover-top-quality-car-rentals-at.html" },
+        { title: "Featured", url: "https://brotherrents.blogspot.com/2024/07/discover-top-quality-car-rentals-at.html" },
+        { title: "Partnership", url: "https://brotherrents.blogspot.com/2024/07/discover-top-quality-car-rentals-at.html" },
+        { title: "Bussiness Relation", url: "https://brotherrents.blogspot.com/2024/07/discover-top-quality-car-rentals-at.html" },
+      ],
+    },
+    {
+      title: "Categories",
+      links: [
+        { title: "Suv", url: "/vehicals?category=suv" },
+        { title: "Standard", url: "/vehicals?category=standard" },
+        { title: "Budget", url: "/vehicals?category=budget" },
+       
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Whattsapp", url: "https://wa.me/923331493112" },
+        { title: "Instagram", url: "/" },
+        { title: "Facebook", url: "/" },
+      ],
+    }
+  ];
+export const phonenumber="923331493112"
